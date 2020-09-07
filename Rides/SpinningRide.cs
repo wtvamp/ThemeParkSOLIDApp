64a4678b61fb4b2b88c8d5f78b98cc99d642f019
Warren J Thompson <warrenjthompson@gmail.com>
2020-09-07 12:45:38 -0700

    Spelling error

--- Rides/SpinningRide.cs
+++ Rides/SpinningRide.cs
@@ -1,15 +1,15 @@
 public class SpinningRide : ThemeParkRide
 {
     private float spinningDegree;
-    private float spinnigSpeed;
+    private float spinningSpeed;
     public SpinningRide(string name, float speed, float spinningDegree, float spinningSpeed): base(name, speed)
     {
-        this.spinnigSpeed = spinnigSpeed;
+        this.spinningSpeed = spinningSpeed;
         this.spinningDegree = spinningDegree;
     }
     public override string ExtraDetails()
     {
         return this.Name + " is a spinning ride that spins " + this.spinningDegree.ToString() +
-        " degrees at " + this.spinnigSpeed.ToString() + "mph.\n";
+        " degrees at " + this.spinningSpeed.ToString() + "mph.\n";
     }
 }
