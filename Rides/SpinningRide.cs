44b85e05368919fd655ab5955ffe3cd70e98bd0a
Warren Thompson <wthompson@wthompson-ltm.home>
2020-09-20 21:35:56 -0700

    Added spinning engine to spinning ride.

--- Rides/SpinningRide.cs
+++ Rides/SpinningRide.cs
@@ -2,13 +2,15 @@ public class SpinningRide : ThemeParkRide
 {
     private float spinningDegree;
     private float spinningSpeed;
-
+    
+    private ISpinningEngine spinningEngine;
     private IExtraDetails extraDetails;
     public SpinningRide(string name, float speed, float spinningDegree, float spinningSpeed): base(name, speed)
     {
         this.spinningSpeed = spinningSpeed;
         this.spinningDegree = spinningDegree;
         this.extraDetails = new ExtraDetails(this.ExtraDetails());
+        this.spinningEngine = new SpinningEngine();
     }
     private string ExtraDetails()
     {
